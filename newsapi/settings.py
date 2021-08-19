1ecf1a985c8109de7d595f8eb96f274297130db0
pravee42 <darkparadise877@gmail.com>
2021-08-19 17:21:19 +0530

    inital commit

--- newsapi/settings.py
+++ newsapi/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
