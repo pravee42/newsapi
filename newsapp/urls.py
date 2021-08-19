4fb991ac32ceda2fc796408998993017c7d8624e
pravee42 <darkparadise877@gmail.com>
2021-08-19 17:24:58 +0530

    inital commit

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.UserInfo, name='Api DOCS'),
-    path('/news', views.getEnglishnews, name='getEnglishnews'),
-    path('/tech', views.getTechnews, name='getTechnews - Abipravi'),
+    path('news', views.getEnglishnews, name='getEnglishnews'),
+    path('tech', views.getTechnews, name='getTechnews - Abipravi'),
 
 ]
