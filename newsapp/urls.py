c24ff7caa61d4b5879181a6c52154bae7ba4a073
pravee42 <darkparadise877@gmail.com>
2021-10-05 07:51:53 +0530

    initial commmit

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -5,7 +5,7 @@ from newsapp.trending import trending
 urlpatterns = [
     path('', views.UserInfo, name='Api DOCS'),
     path('sports',views.getSportsNews, name="getSportsNews"),
-    path('latestsports',views.latestSportsnews, name="latestSportsnews"),
+    path('sports/latest',views.latestSportsnews, name="latestSportsnews"),
     path('news', views.getEnglishnews, name='getEnglishnews'),
     path('tech', views.getTechnews, name='getTechnews - Abipravi'),
     path('business', businessnews.business, name='business'),
