620ddb4df8c981d6bd650af8054f33060790e6fe
ABIPRAVI <65100038+pravee42@users.noreply.github.com>
2021-10-15 10:56:58 +0530

    Update settings.py

--- newsapi/settings.py
+++ newsapi/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 #CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
-   'http://localhost:3000/business',
+   'http://localhost:3000/',
 'http://newsapp-abipravi.netlify.app/',
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
