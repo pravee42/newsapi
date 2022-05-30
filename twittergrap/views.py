47d02c10c3e603a75ee41c9888a24b39f7373a3e
ABIPRAVI <darkparadise877@gmail.com>
2022-05-30 14:12:04 +0530

    Updated build pack

--- twittergrap/views.py
+++ twittergrap/views.py
@@ -8,14 +8,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import os
 
 
 @api_view(['GET'])
 def getData(request, pk):
-    s = Service(ChromeDriverManager().install())
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    bot = webdriver.Chrome(executable_path=os.environ.get(
+        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    # s = Service(ChromeDriverManager().install())
     url = 'https://twitter.com/' + pk
     photo_url = url + '/photo'
-    bot = webdriver.Chrome(service=s)
+    # bot = webdriver.Chrome(service=s)
     wait = WebDriverWait(bot, 20)
     bot.get(url)
     user_name = wait.until(
